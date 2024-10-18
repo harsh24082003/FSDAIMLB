c420df9ab26ad7da7f66bf4c965319092d0414d4
harsh24082003 <hs3935873@gmail.com>
2024-10-18 08:55:48 +0530

    JS Introduction

---
+++ JSWORK/index.js
@@ -0,0 +1,21 @@
+// console.log("Hello JS");
+
+// const a=12;
+// if(a>10){
+//     let b=23;
+//     a=40;
+//     console.log("a="+a);
+// }
+// console.log("Hi, a="+b);
+
+// function fun(){
+//     console.log("Heeee");
+// }
+// fun();
+
+function fun(a,b){
+    console.log("heee")
+    return a+b;
+}
+let data=fun(2,4);
+console.log(data);
